ccfe9b47be4f925d9b5020448f07848f6cac730d
Job <jalblas@ilionx.com>
2025-09-13 09:23:56 +0200

    Remove click me demo

--- composeApp/src/commonMain/kotlin/org/energy/pricing/App.kt
+++ composeApp/src/commonMain/kotlin/org/energy/pricing/App.kt
@@ -1,7 +1,5 @@
 package org.energy.pricing
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,23 +10,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.energy.pricing.data.InMemoryStore
 import org.energy.pricing.importer.parseCsvForImport
 import org.energy.pricing.importer.pickCsvFileContent
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import dynamicpricesim.composeapp.generated.resources.Res
-import dynamicpricesim.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -36,20 +29,6 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-
             Divider()
             // CSV Upload Section
             Text("CSV Import", style = MaterialTheme.typography.titleMedium)
