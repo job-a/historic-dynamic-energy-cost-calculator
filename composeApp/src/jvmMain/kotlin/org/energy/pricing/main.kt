69f4ea5bb21d49aa5cc88d752c0d93fc022edaf4
Job <jalblas@ilionx.com>
2025-09-13 11:31:24 +0200

    max window on start

--- composeApp/src/jvmMain/kotlin/org/energy/pricing/main.kt
+++ composeApp/src/jvmMain/kotlin/org/energy/pricing/main.kt
@@ -1,12 +1,15 @@
 package org.energy.pricing
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "DynamicPriceSim",
+        state = rememberWindowState(placement = WindowPlacement.Maximized),
     ) {
         App()
     }
